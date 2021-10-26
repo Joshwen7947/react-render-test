698ace76028a61a360a444e2dd0dd7845f2c8bd6
Josh <joshuawen97@gmail.com>
2021-10-26 18:25:39 +0700

    cleaned up folder

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ import AddTen from './components/AddTen';
 import MinusTen from './components/MinusTen';
 function App() {
 	const [count, setCount] = useState(0);
-	// const [email, setEmail] = useState('');
-	// const handleSubmit = (event) => {
-	// 	event.preventDefault();
-	// };
-	// const handleOnChange = (event) => {
-	// 	console.log(`event`, event);
-	// };
-	// console.log(`re-render`);
 
 	return (
 		<div className="App">
@@ -32,21 +24,3 @@ function App() {
 }
 
 export default App;
-{
-	/* <div style={{ marginTop: '5rem', fontSize: '30px' }}>
-				<form
-					action="" //onSubmit={handleSubmit}//
-				>
-					<label for="email">Email:</label>
-					<input type="text" id="email" />
-					<br />
-					<label for="password">Password:</label>
-					<input type="password" id="password" />
-					<br />
-					<button
-						type="submit"
-						value="submit" //onClick={handleSubmit}
-					></button>
-				</form> 
-			</div> */
-}
